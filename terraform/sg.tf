3bea1fa8c5cd973a974f10fc01aacef6154aa723
salgu1998 <salgu1998@gmail.com>
2023-04-02 16:58:58 +0900

    add: 보안그룹에 outbound 모든 프로토콜 다 허용

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -11,3 +11,21 @@ resource "aws_security_group_rule" "cicd_sg_ingress_ssh" {
   cidr_blocks = ["218.237.59.119/32"]
   security_group_id = aws_security_group.cicd_sg.id
 }
+
+resource "aws_security_group_rule" "cicd_sg_ingress_https" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.cicd_sg.id
+}
+
+resource "aws_security_group_rule" "cicd_sg_egress_all" {
+  type             = "egress"
+  from_port        = 0
+  to_port          = 0
+  protocol         = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.cicd_sg.id
+}
